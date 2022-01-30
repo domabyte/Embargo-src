4d1b57506955682040f423f61a7a454ab84de368
Dikshit <singhdikshit69@gmail.com>
2022-01-30 21:48:37 +0530

    updating src third time

--- components/Ride/maps/Map.jsx
+++ components/Ride/maps/Map.jsx
@@ -7,6 +7,8 @@ import AnimationLottie1 from "../../../lotties/manbike.json";
 import AnimationLottie3 from "../../../lotties/reserve1.json";
 import AnimationLottie4 from "../../../lotties/car1.json";
 import AnimationLottie from "../../../AnimationLottie";
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+import { IconButton } from '@mui/material';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGlrc2hpdDIzMzJrIiwiYSI6ImNrd2RmZzZmeDI2bXAyb3BhZjF1NzE2dXUifQ.uO1S9-rLDi9Izo3vFVIgEg';
 
@@ -58,12 +60,12 @@ export default function Map() {
                 </div>
                     </div>
                 </div>
-                <div className='action'>
-                <div className='action_child' onClick={()=>setChange(true)}>
+                {(change)?(<><div className='action'>
+                <div className='action_child' onClick={()=>setChange(false)}>
                 <AnimationLottie name={AnimationLottie4} height={115} width={160}/>
                     <span>Car</span>
                 </div>
-                <div className='action_child' onClick={()=>setChange(true)}>
+                <div className='action_child' onClick={()=>setChange(false)}>
                 <AnimationLottie name={AnimationLottie1} height={115} width={160}/>
                     <span>Bike</span>
                 </div>
@@ -73,13 +75,51 @@ export default function Map() {
                 </div>
                 </div>
                 <div className='destination'>
-                   <span>Choose one of them</span>
+                   <span>All at one place</span>
                 </div>
                 <div className='powered'>
                     <p style={{fontSize:"14px"}}>
-                        Powered by embargo
+                        Powered by <span style={{fontSize:"16px",fontWeight:"600"}}>Embargo</span>
                     </p>
-                </div>
+                </div></>):(<>
+                    <div className='sec_containerPr'>
+                        <div className="backbutton">
+                        <IconButton onClick={()=>setChange(true)}>
+                            <KeyboardBackspaceIcon fontSize='large'/>
+                        </IconButton>
+                        </div>
+                        <div className='sec_container'>
+                        <div className="input">
+                            <div className='circle'>
+                            <img src='https://img.icons8.com/ios/50/9CA3AF/filled-circle.png' alt='circle'/>
+                            </div>
+                            <div className='line'>
+                            <img src='https://img.icons8.com/ios/50/9CA3AF/vertical-line.png' alt='line'/>
+                            </div>
+                            <div className='square'>
+                            <img src='https://img.icons8.com/windows/32/000000/square-full.png' alt='square'/>
+                            </div>
+                            </div>
+                            <div className='type'>
+                            <input type="text" placeholder='Enter pickup Location'/>
+                            <input type="text" placeholder='Where to?'/>
+                            </div>
+                            </div>
+                        <div className="savedPlaces">
+                        <img src="https://img.icons8.com/material-outlined/24/000000/star--v2.png" alt="star"/>
+                        <p>
+                        Saved Places
+                        </p>
+                        </div>
+                        <div className="confirmLocation">
+                        <div className="confirm_button">
+                            <a href='#/' text="confirm now" className='confirm'>
+                                Confirm Locations
+                            </a>
+                            </div>
+                        </div>
+                    </div>
+                </>)}
             </div>
             </>
     );
